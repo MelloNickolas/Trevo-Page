034479514374c6e5b111d84e7ffc5545c4c9e968
Nickolas Passos Homem de Mello <nickolasphmello10@gmail.com>
2025-10-30 17:45:11 -0300

    Home Finished
    The home page has been conclused

---
+++ js/carros-inicio.js
@@ -0,0 +1,40 @@
+const carrosLista = document.getElementById('carros-lista');
+
+function criarCard(carro) {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = `
+    <img src="${carro.imagens['foto-inicio']}" alt="${carro.nome}">
+    <div class="card-title">
+      <h2 class="font-league-24-bold neutro-8">${carro.nome}</h2>
+    </div>
+    <div class="card-description">
+      <h4 class="font-league-14-light neutro-6">${carro.descricao}</h4>
+    </div>
+    <div class="card-topics">
+      <div class="topic">
+        <img src="../assets/imgs/Calendar.svg" height="20px">
+        <h5 class="font-league-12 neutro-9">${carro.ano}</h5>
+      </div>
+      <div class="topic">
+        <img src="../assets/imgs/Value.svg" height="20px">
+        <h5 class="font-league-12 neutro-9">${carro.preco}</h5>
+      </div>
+    </div>
+    <div class="card-buttons">
+      <a href="" class="font-league-12 neutro-6">Ver Mais</a>
+      <a href="" class="font-league-12 neutro-0">Comprar</a>
+    </div>
+  `;
+  return card;
+}
+
+fetch('../dados/Carros-Inicio.json')
+  .then(response => response.json())
+  .then(carros => {
+    // Cria os cards
+    carros.forEach(carro => carrosLista.appendChild(criarCard(carro)));
+    // Duplica os mesmos pra criar o looping contínuo
+    carros.forEach(carro => carrosLista.appendChild(criarCard(carro)));
+  })
+  .catch(error => console.error('Erro ao carregar os carros:', error));
